616d04aa676c18c60fa5fcca7d11689023bf453a
aymen <40662254+aymenkasP@users.noreply.github.com>
2022-05-10 17:58:14 +0100

    Update _document.js
    Delete snipcart css

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
         <Head >
         <link rel="preconnect" href="https://app.snipcart.com" />
         <link rel="preconnect" href="https://cdn.snipcart.com" />
-        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.3.1/default/snipcart.css" />
+        
         </Head>
         <body>
           <Main />
